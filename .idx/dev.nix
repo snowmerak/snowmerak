d5c79fae4391bc43bacc78d60755e4388ae2bb4d
myharaei <myharaei@gmail.com>
2024-11-05 06:44:49 +0000

    update extension

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -21,6 +21,7 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
+      "yzhang.markdown-all-in-one"
     ];
 
     # Enable previews
